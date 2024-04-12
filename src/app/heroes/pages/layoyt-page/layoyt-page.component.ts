e96d788345f59fda0757f3127cf17b53de0cb83e
Raynel Feliz <theraynel@gmail.com>
2024-04-11 20:25:22 -0400

    first commit

---
+++ src/app/heroes/pages/layoyt-page/layoyt-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layoyt-page',
+  templateUrl: './layoyt-page.component.html',
+  styles: [
+  ]
+})
+export class LayoytPageComponent {
+
+}
