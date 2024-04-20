48993fbf17c8a37a2a41257688ae96c47c665dbe
Raynel Feliz <theraynel@gmail.com>
2024-04-20 17:59:04 -0400

    new-page complited

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,29 +1,117 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Publisher } from '../../interfaces/hero.interface';
+import { ActivatedRoute, Router } from '@angular/router';
+import { filter, switchMap } from 'rxjs';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class NewPageComponent {
-
+export class NewPageComponent implements OnInit {
   public heroForm = new FormGroup({
-    id:               new FormControl<string>(''),
-    superhero:        new FormControl<string>('', { nonNullable: true }),
-    publisher:        new FormControl<Publisher>(Publisher.DCComics),
-    alter_ego:        new FormControl(''),
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
     first_appearance: new FormControl(''),
-    characters:       new FormControl(''),
-    alt_img:          new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
   });
 
+  public publishers = [
+    { id: 'DC Comics', desc: 'DC - Comics' },
+    { id: 'Marvel Comics', desc: 'Marvel - Comics' },
+  ];
 
+  constructor(
+    private heroServices: HeroesService,
+    private activaredRouter: ActivatedRoute,
+    private router: Router,
+    private snackbar: MatSnackBar,
+    private dialog: MatDialog
+  ) {}
 
- public publishers = [
-   {id: 'DC Comics', desc: 'DC - Comics'},
-   {id: 'Marvel Comics', desc: 'Marvel - Comics'},
-]
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
+  ngOnInit(): void {
+
+    if ( !this.router.url.includes('edit') ) return;
+
+    this.activaredRouter.params
+      .pipe(
+         switchMap( ({ id }) => this.heroServices.getHeroById( id ) ),
+      ).subscribe( hero => {
+
+        if ( !hero) return this.router.navigateByUrl('/');
+
+        this.heroForm.reset( hero );
+
+        return;
+
+      })
+
+  }
+
+  onSubmit(): void {
+    if (this.heroForm.invalid) return;
+
+    if (this.currentHero.id) {
+      this.heroServices.updateHero(this.currentHero).subscribe((hero) => {
+        this.showSnackBar(`${ hero.superhero } updated!`);
+      });
+
+      return;
+    }
+
+    this.heroServices.addHero(this.currentHero).subscribe((hero) => {
+      // TODo: montrar snackbar y navegar a /heroes/edit/ hero.id
+      this.router.navigate( ['/heroes/edit', hero.id ])
+      this.showSnackBar(`${ hero.superhero } created!`);
+    });
+  }
+
+  onDeleteHero(){
+    if ( !this.currentHero.id ) throw new Error('Hero idis required')
+
+      const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+        data: this.heroForm.value,
+      });
+
+      dialogRef.afterClosed()
+      .pipe(
+        filter( (result: boolean) => result),
+        switchMap( () => this.heroServices.deleteHeroById( this.currentHero.id )),
+        filter( (deleted: boolean) => deleted ),
+      )
+      .subscribe(() => {
+         this.router.navigate(['/heroes'])
+      });
+
+      // dialogRef.afterClosed().subscribe(result => {
+      //   if ( !result) return;
+
+      //   this.heroServices.deleteHeroById( this.currentHero.id )
+      //   .subscribe(deleted => {
+      //     if( deleted )
+      //       this.router.navigate(['/heroes']);
+      //   });
+      // });
+  }
+
+  showSnackBar( message: string ):void {
+     this.snackbar.open( message, 'done', {
+      duration: 2500
+     });
+  }
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -8,40 +8,37 @@ import { environments } from 'src/environments/environments';
 export class HeroesService {
   private baseUrl: string = environments.baseUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
   }
 
   getHeroById(id: string): Observable<Hero | undefined> {
-    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
-      .pipe(
-        catchError(error => of(undefined))
-      )
+    return this.http
+      .get<Hero>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(catchError((error) => of(undefined)));
   }
 
   getsuggestions(query: string): Observable<Hero[]> {
-
     const url: string = `${this.baseUrl}/heroes?q=${query}&_limit=6`;
     return this.http.get<Hero[]>(url);
   }
 
-  addHero( hero: Hero ):Observable<Hero> {
-     return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
   }
 
-  updateHero( hero: Hero ): Observable<Hero> {
-     if ( !hero.id ) throw new Error('Hero id is required');
+  updateHero(hero: Hero): Observable<Hero> {
+    if (!hero.id) throw new Error('Hero id is required');
 
-     return this.http.patch<Hero>(`${this.baseUrl}/heroes`, hero);
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
   }
 
-  deleteHeroById( id: string ): Observable<boolean> {
-     return this.http.delete(`${this.baseUrl}/heroes/${id}`)
-        .pipe(
-           catchError( err => of( false ) ),
-           map( resp => true )
-        );
+  deleteHeroById(id: string): Observable<boolean> {
+    return this.http.delete(`${this.baseUrl}/heroes/${id}`).pipe(
+      map((resp) => true),
+      catchError((err) => of(false))
+    );
   }
 }
